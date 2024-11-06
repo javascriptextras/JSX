d9ad45834a1e9b633699f5d441d54c0816b3aa3b
mimmeer <175632698+mimmeer-github@users.noreply.github.com>
2024-11-06 18:49:30 +0000

    Update dom.js

--- jsx/dom.js
+++ jsx/dom.js
@@ -5,3 +5,14 @@ export function replaceHTML(ID, INNER) {
 export function replaceATTRIB(ID, ATTRIB, VAL) {
   document.getElementById(ID)[ATTRIB] = VAL;
 }
+
+export function addHTML(parentId, html) {
+  const parentElement = document.getElementById(parentId);
+  const div = document.createElement('div');
+  div.innerHTML = html;
+  const children = div.childNodes;
+
+  for (let i = 0; i < children.length; i++) {
+    parentElement.appendChild(children[i]);
+  }
+}
